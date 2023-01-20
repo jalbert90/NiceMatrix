c064edb48c901ea1c6d0c5bdcf49109d176a4a9b
jacob <jalbert90@gmail.com>
2023-01-19 22:45:35 -0500

    I have a bug. Probably not solving correctly, anyways.

--- firstAttempt/firstAttempt.cpp
+++ firstAttempt/firstAttempt.cpp
@@ -8,28 +8,39 @@ using namespace std;
 
 ll int solve() {
 	int n, m;
+	ll int ans = 0;
 	cin >> n >> m;
 
 	vector<vector<int>> mt(n, vector<int>(m));
-	vector<int> l;
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			int input;
 			cin >> input;
 			mt[i][j] = input;
-			l.push_back(input);
 		}
 	}
 
-	sort(l.begin(), l.end());
-
-	int midIndex = (n * m) / 2;
-	int midNum = l[midIndex];
-	ll int ans = 0;
-
-	for (int i = 0; i < n * m; i++) {
-		ans += abs(l[i] - midNum);
+	for (int i = 0; i < (n + 1) / 2; i++) {
+		for (int j = 0; j < (m + 1) / 2; j++) {
+			ll int avg = (mt[i][j] + mt[n - i - 1][j] + mt[i][m - j - 1] + mt[n - i - 1][m - j - 1]) / 4;
+			ll int absDiffTR = abs(mt[i][m - j - 1] - avg);
+			if (absDiffTR != 0) {
+				ans += absDiffTR;
+			}
+			ll int absDiffBL = abs(mt[n - i - 1][j] - avg);
+			if (absDiffBL != 0) {
+				ans += absDiffBL;
+			}
+			ll int absDiffBR = abs(mt[n - i - 1][m - j - 1] - avg);
+			if (absDiffBR != 0) {
+				ans += absDiffBR;
+			}
+			ll int absDiffTL = abs(mt[i][j] - avg);
+			if (absDiffTL != 0) {
+				ans += absDiffTL;
+			}
+		}
 	}
 
 	return ans;
