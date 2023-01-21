d7093e813c48f5fa38c4130a4941f470781af783
jacob <jalbert90@gmail.com>
2023-01-20 23:36:19 -0500

    Found bug. Overcounting middle elements. Will fix tomorrow.

--- firstAttempt/firstAttempt.cpp
+++ firstAttempt/firstAttempt.cpp
@@ -22,22 +22,28 @@ ll int solve() {
 	}
 
 	for (int i = 0; i < (n + 1) / 2; i++) {
+		if (i == 0) continue;
 		for (int j = 0; j < (m + 1) / 2; j++) {
+			if (j == 0) continue;
 			ll int avg = (mt[i][j] + mt[n - i - 1][j] + mt[i][m - j - 1] + mt[n - i - 1][m - j - 1]) / 4;
 			ll int absDiffTR = abs(mt[i][m - j - 1] - avg);
 			if (absDiffTR != 0) {
+				cout << absDiffTR << endl;
 				ans += absDiffTR;
 			}
 			ll int absDiffBL = abs(mt[n - i - 1][j] - avg);
 			if (absDiffBL != 0) {
+				cout << absDiffBL << endl;
 				ans += absDiffBL;
 			}
 			ll int absDiffBR = abs(mt[n - i - 1][m - j - 1] - avg);
 			if (absDiffBR != 0) {
+				//cout << absDiffBR << endl;
 				ans += absDiffBR;
 			}
 			ll int absDiffTL = abs(mt[i][j] - avg);
 			if (absDiffTL != 0) {
+				//cout << absDiffTL << endl;
 				ans += absDiffTL;
 			}
 		}
